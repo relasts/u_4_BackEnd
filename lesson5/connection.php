249246fb001718dfe69d7a3b0e04008f701a9f5f
relasts <tsovyan@inbox.ru>
2024-04-30 10:08:42 +0300

    lesson4s

--- lesson5/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // $db = new PDO('mysql:host=localhost;dbname=u67404', 'root', '');
-    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $db = new PDO('mysql:host=localhost;dbname=u67404', 'u67404', '4971288',
-    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-?>
